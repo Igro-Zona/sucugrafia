cb738014d68245f5d5dfd1c56bea5b432ab44b74
Igro-Zona <tim.bic22@gmail.com>
2025-11-17 05:49:27 +0300

    created own Modal and added to display galery images

--- app/pages/galery.vue
+++ app/pages/galery.vue
@@ -8,14 +8,19 @@
 				class="my-4 grid h-full auto-rows-max grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3"
 				:as-child="true"
 			>
-				<NuxtPicture
+				<button
 					v-for="src in images"
 					:key="src"
-					:src="`/${src}`"
-					sizes="sm:400px md:500px lg:600px"
-					class="overflow-hidden rounded-xl object-cover"
-					:img-attrs="{ alt: 'Gallery image', loading: 'lazy', class: 'w-full aspect-video  ' }"
-				/>
+					class="cursor-pointer"
+					@click="open(src)"
+				>
+					<NuxtPicture
+						:src="`/${src}`"
+						sizes="sm:400px md:500px lg:600px"
+						class="block overflow-hidden rounded-xl object-cover"
+						:img-attrs="{ alt: 'Gallery image', loading: 'lazy', class: 'w-full aspect-video  ' }"
+					/>
+				</button>
 			</ScrollAreaViewport>
 			<ScrollAreaScrollbar class="bg-muted w-2.5 rounded-2xl p-[1.5px]">
 				<ScrollAreaThumb class="bg-secondary cursor-grab rounded-2xl" />
@@ -26,6 +31,16 @@
 
 <script setup lang="ts">
 import { ScrollAreaRoot, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport } from "reka-ui";
+import ImageModal from "~/components/ImageModal.vue";
 
 const images = Array.from({ length: 8 }, (_, i) => `img${i + 1}.jpg`);
+
+const overlay = useOverlay();
+const modal = overlay.create(ImageModal);
+
+async function open(src: string) {
+	modal.open({
+		src: src,
+	});
+}
 </script>
