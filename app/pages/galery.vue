65cdf284c33f126a2f30e4e6785f3521ac01ecc2
Igro-Zona <tim.bic22@gmail.com>
2025-11-17 04:44:10 +0300

    added images to the galery

--- app/pages/galery.vue
+++ app/pages/galery.vue
@@ -5,19 +5,20 @@
 			style="--scrollbar-size: 10px"
 		>
 			<ScrollAreaViewport
-				class="my-4 grid h-full grid-cols-1 gap-4 px-4 lg:grid-cols-2"
+				class="my-4 grid h-full auto-rows-max grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3"
 				:as-child="true"
 			>
-				<div
-					v-for="tag in tags"
-					:key="tag"
-					class="text-mauve12 border-t-mauve6 bg-muted h-80 px-4 pt-2.5 text-xs leading-[18px]"
-				>
-					{{ tag }}
-				</div>
+				<NuxtPicture
+					v-for="src in images"
+					:key="src"
+					:src="`/${src}`"
+					sizes="sm:400px md:500px lg:600px"
+					class="overflow-hidden rounded-xl object-cover"
+					:img-attrs="{ alt: 'Gallery image', loading: 'lazy', class: 'w-full aspect-video  ' }"
+				/>
 			</ScrollAreaViewport>
 			<ScrollAreaScrollbar class="bg-muted w-2.5 rounded-2xl p-[1.5px]">
-				<ScrollAreaThumb class="bg-secondary translate-x-px cursor-grab rounded-2xl" />
+				<ScrollAreaThumb class="bg-secondary cursor-grab rounded-2xl" />
 			</ScrollAreaScrollbar>
 		</ScrollAreaRoot>
 	</AppPage>
@@ -26,5 +27,5 @@
 <script setup lang="ts">
 import { ScrollAreaRoot, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport } from "reka-ui";
 
-const tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`);
+const images = Array.from({ length: 8 }, (_, i) => `img${i + 1}.jpg`);
 </script>
