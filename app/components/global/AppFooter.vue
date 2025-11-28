e256d62b5aa12bba1934785bf863e1fe4d6fc1b3
Igro-Zona <tim.bic22@gmail.com>
2025-11-28 05:38:15 +0300

    customizing container and footer layout

--- app/components/global/AppFooter.vue
+++ app/components/global/AppFooter.vue
@@ -1,9 +1,67 @@
 <template>
-	<UFooter>
-		<template #left>
+	<footer class="border-primary mt-4 border-t lg:mt-8">
+		<UiContainer>
+			<UFooterColumns
+				:columns
+				class="py-8 lg:py-12"
+			/>
+		</UiContainer>
+		<UiContainer class="border-muted flex flex-col gap-3 border-t py-4 lg:flex-row">
 			<p class="text-muted text-sm">
-				Copyright © {{ new Date().getFullYear() }} | Sucugrafia - todos derechos reservados
+				Copyright © {{ new Date().getFullYear() }} | Todos los derechos reservados.
 			</p>
-		</template>
-	</UFooter>
+		</UiContainer>
+	</footer>
 </template>
+
+<script setup lang="ts">
+import type { FooterColumn } from "@nuxt/ui";
+
+const columns: FooterColumn[] = [
+	{
+		label: "Community",
+		children: [
+			{
+				label: "Nuxters",
+				to: "https://nuxters.nuxt.com",
+				target: "_blank",
+			},
+			{
+				label: "Video Courses",
+				to: "https://masteringnuxt.com/nuxt3?ref=nuxt",
+				target: "_blank",
+			},
+			{
+				label: "Nuxt on GitHub",
+				to: "https://github.com/nuxt",
+				target: "_blank",
+			},
+		],
+	},
+	{
+		label: "Solutions",
+		children: [
+			{
+				label: "Nuxt Content",
+				to: "https://content.nuxt.com/",
+				target: "_blank",
+			},
+			{
+				label: "Nuxt DevTools",
+				to: "https://devtools.nuxt.com/",
+				target: "_blank",
+			},
+			{
+				label: "Nuxt Image",
+				to: "https://image.nuxt.com/",
+				target: "_blank",
+			},
+			{
+				label: "Nuxt UI",
+				to: "https://ui.nuxt.com/",
+				target: "_blank",
+			},
+		],
+	},
+];
+</script>
---
+++ app/components/ui/UiContainer.vue
@@ -0,0 +1,13 @@
+<template>
+	<div :class="twMerge('mx-auto w-full max-w-(--ui-container) px-4 sm:px-6 lg:px-8', props.class)">
+		<slot />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge";
+
+const props = defineProps<{
+	class?: string;
+}>();
+</script>
