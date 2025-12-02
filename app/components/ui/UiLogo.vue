ce996be6c2ab6ca358530d7635d3c82bb8a53974
Igro-Zona <tim.bic22@gmail.com>
2025-12-02 04:26:20 +0300

    created UiLogo component

---
+++ app/components/ui/UiLogo.vue
@@ -0,0 +1,25 @@
+<template>
+	<Primitive
+		:class="twMerge('font-main text-primary flex items-center gap-1 text-2xl font-semibold', className)"
+		:as
+	>
+		<Icon
+			mode="svg"
+			name="i-sucua-book"
+			class="text-primary"
+			size="34"
+		/>
+		<span><span class="text-default">Sucu</span>grafia</span>
+	</Primitive>
+</template>
+
+<script setup lang="ts">
+import type { AsTag } from "reka-ui";
+import { Primitive } from "reka-ui";
+import { twMerge } from "tailwind-merge";
+
+const { class: className = "", as = "div" } = defineProps<{
+	class?: string;
+	as?: AsTag | Component;
+}>();
+</script>
