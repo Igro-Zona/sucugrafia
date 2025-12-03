69d15b5bc879641150e97c010f743d558208ad19
Igro-Zona <tim.bic22@gmail.com>
2025-12-03 04:55:47 +0300

    refactored AppFooter component

--- app/components/global/AppFooter.vue
+++ app/components/global/AppFooter.vue
@@ -1,19 +1,84 @@
 <template>
-	<footer class="border-muted mt-4 border-t lg:mt-8">
-		<UiContainer>
+	<footer
+		class="border-muted mt-4 border-t lg:mt-8"
+		aria-labelledby="footer-label"
+	>
+		<h2
+			id="footer-label"
+			class="sr-only"
+		>
+			Pie de página
+		</h2>
+
+		<UiContainer class="flex auto-cols-fr grid-flow-col flex-col gap-8 lg:grid xl:col-span-2">
 			<UFooterColumns
 				:columns
 				class="py-8 lg:py-12"
 			/>
+			<nav
+				aria-label="Navegación"
+				class="py-8 text-sm lg:py-12"
+			>
+				<h3 class="font-semibold">Navegación</h3>
+
+				<ul class="text-muted mt-6 space-y-4">
+					<li class="hover:text-default">
+						<UiLink to="https://nuxters.nuxt.com">Nuxters</UiLink>
+					</li>
+					<li class="hover:text-default">
+						<UiLink to="https://nuxters.nuxt.com">Nuxters</UiLink>
+					</li>
+					<li class="hover:text-default">
+						<UiLink to="https://nuxters.nuxt.com">Nuxters</UiLink>
+					</li>
+					<li class="hover:text-default">
+						<UiLink to="https://nuxters.nuxt.com">Nuxters</UiLink>
+					</li>
+				</ul>
+			</nav>
 		</UiContainer>
 
 		<USeparator color="primary" />
 
-		<UiContainer class="flex flex-col items-center gap-3 py-4 lg:flex-row">
-			<UiLogo />
-			<p class="text-muted text-sm">
-				Copyright © {{ new Date().getFullYear() }} | Todos los derechos reservados.
-			</p>
+		<UiContainer class="flex flex-col items-center justify-between gap-3 py-4 lg:flex-row">
+			<div class="flex flex-col items-center gap-3 lg:flex-row">
+				<div class="flex items-center gap-6">
+					<UiLink
+						to="/"
+						:styled="false"
+					>
+						<UiLogo />
+					</UiLink>
+
+					<UiLink
+						to="https://github.com/Igro-Zona/sucugrafia"
+						:styled="false"
+						class="hover:bg-muted active:bg-accented rounded-lg p-2 lg:hidden"
+					>
+						<Icon
+							name="i-simple-icons-github"
+							size="30"
+							class="text-muted"
+						/>
+					</UiLink>
+				</div>
+
+				<p class="text-muted text-sm">
+					Copyright © {{ new Date().getFullYear() }} | Todos los derechos reservados.
+				</p>
+			</div>
+
+			<UiLink
+				to="https://github.com/Igro-Zona/sucugrafia"
+				:styled="false"
+				class="hover:bg-muted active:bg-accented hidden rounded-lg p-2 lg:inline-flex"
+			>
+				<Icon
+					name="i-simple-icons-github"
+					size="30"
+					class="text-muted"
+				/>
+			</UiLink>
 		</UiContainer>
 	</footer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
 			name: "page",
 			mode: "out-in",
 		},
+		baseURL: "/",
 	},
 
 	components: [
