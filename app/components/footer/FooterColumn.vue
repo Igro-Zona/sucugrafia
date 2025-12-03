7626e58ef5d0efe1e0c5decaf292a1d81f5aeb42
Igro-Zona <tim.bic22@gmail.com>
2025-12-03 06:01:03 +0300

    created FooterColumn component

---
+++ app/components/footer/FooterColumn.vue
@@ -0,0 +1,36 @@
+<template>
+	<nav
+		:aria-label="props.label"
+		class="py-8 text-sm lg:py-12"
+	>
+		<h3 class="font-semibold">{{ props.label }}</h3>
+
+		<ul
+			v-if="children"
+			class="text-muted mt-6 space-y-4"
+		>
+			<li
+				v-for="(item, index) in props.children"
+				:key="index"
+				class="hover:text-default cursor-pointer transition-colors"
+			>
+				<UiLink v-bind="item">{{ item.label }}</UiLink>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script setup lang="ts">
+import type { NuxtLinkProps } from "#app";
+
+type ChildrenProps = {
+	label: string;
+} & NuxtLinkProps;
+
+const props = defineProps<{
+	label: string;
+	children?: ChildrenProps[];
+}>();
+</script>
+
+<style scoped></style>
