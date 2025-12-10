bca18a55a7c889fde93d513f4ec76c931c861a9b
Igro-Zona <tim.bic22@gmail.com>
2025-12-10 04:56:00 +0300

    extracted some ui in components

---
+++ app/components/ui/UiGrid.vue
@@ -0,0 +1,13 @@
+<template>
+	<div :class="twMerge('grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3', className)">
+		<slot />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge";
+
+const { class: className = "" } = defineProps<{
+	class?: string;
+}>();
+</script>
