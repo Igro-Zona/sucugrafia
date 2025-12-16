db48602da48fc064c8a1e5a61aab01fb442021fd
Igro-Zona <tim.bic22@gmail.com>
2025-12-16 03:25:00 +0300

    created UiSkeleton.vue component

---
+++ app/components/ui/UiSkeleton.vue
@@ -0,0 +1,23 @@
+<template>
+	<Primitive
+		:as="as"
+		aria-busy="true"
+		aria-label="loading"
+		aria-live="polite"
+		role="alert"
+		:class="twMerge('bg-elevated animate-pulse rounded-md', className)"
+	>
+		<slot />
+	</Primitive>
+</template>
+
+<script setup lang="ts">
+import type { AsTag } from "reka-ui";
+import { Primitive } from "reka-ui";
+import { twMerge } from "tailwind-merge";
+
+const { class: className = "", as = "div" } = defineProps<{
+	class?: string;
+	as?: AsTag | Component;
+}>();
+</script>
