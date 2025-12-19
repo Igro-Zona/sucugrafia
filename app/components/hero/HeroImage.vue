de7de9a1881c08a91840185eee98c90a9d307890
Igro-Zona <tim.bic22@gmail.com>
2025-12-19 04:56:17 +0300

    created HeroImage component

---
+++ app/components/hero/HeroImage.vue
@@ -0,0 +1,29 @@
+<template>
+	<NuxtPicture
+		quality="60"
+		width="460"
+		height="259"
+		densities="1x 2x"
+		format="avif,webp"
+		legacy-format="jpg"
+		:src
+		provider="cloudinary"
+		class="block aspect-video w-115"
+		:img-attrs="{
+			alt: '',
+			class: 'rounded-md object-cover w-full aspect-video',
+		}"
+		loading="eager"
+		sizes="460px"
+		:preload="{
+			fetchPriority: 'high',
+		}"
+		placeholder
+	/>
+</template>
+
+<script setup lang="ts">
+const { src } = defineProps<{
+	src: string;
+}>();
+</script>
