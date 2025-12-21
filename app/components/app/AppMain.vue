5ad1e8769c2b4145829923c80b08506f6bb154bc
Igro-Zona <tim.bic22@gmail.com>
2025-12-22 02:12:10 +0300

    fixed bug which cause layout shift on resize

--- app/components/app/AppMain.vue
+++ app/components/app/AppMain.vue
@@ -3,7 +3,7 @@
 		v-spotlight
 		class="box-border min-h-[calc(100vh-var(--ui-header-height)+1px)] pb-8"
 	>
-		<div class="relative z-9">
+		<div class="relative z-9 overflow-hidden">
 			<slot />
 		</div>
 	</main>
