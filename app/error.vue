e51e3831dbedc40be3a3c7f0c64383e90e68afa3
Igro-Zona <tim.bic22@gmail.com>
2025-12-25 19:37:15 +0300

    refactored error route

--- app/error.vue
+++ app/error.vue
@@ -1,17 +1,40 @@
 <template>
-	<UApp>
+	<UApp :locale="es">
 		<AppHeader />
 
-		<UError
-			as="main"
-			:error="error"
-			:clear="{
-				color: 'neutral',
-				size: 'xl',
-				icon: 'i-lucide-arrow-left',
-				class: 'rounded-full',
-			}"
-		/>
+		<AppMain class="flex items-center justify-center">
+			<div class="flex max-w-200 flex-col items-center justify-center p-4">
+				<p
+					v-if="error.statusCode"
+					class="text-primary text-base font-semibold"
+				>
+					{{ error.statusCode }}
+				</p>
+				<h2
+					v-if="error.statusMessage"
+					class="text-highlighted mt-2 text-center text-4xl font-bold sm:text-5xl"
+				>
+					{{ error.statusMessage }}
+				</h2>
+				<p
+					v-if="error.message && error.message !== error.statusMessage"
+					class="text-muted mt-4 text-center text-lg"
+				>
+					{{ error.message }}
+				</p>
+				<UiLink
+					:styled="false"
+					class="text-inverted bg-inverted hover:bg-inverted/80 active:bg-inverted/60 mt-8 flex items-center justify-center gap-2 rounded-full px-3 py-2 font-medium"
+					to="/"
+				>
+					<Icon
+						name="i-lucide-arrow-left"
+						size="24"
+					/>
+					<span>Volver al inicio</span></UiLink
+				>
+			</div>
+		</AppMain>
 
 		<AppFooter />
 	</UApp>
@@ -19,6 +42,7 @@
 
 <script setup lang="ts">
 import type { NuxtError } from "#app";
+import { es } from "@nuxt/ui/locale";
 
 const { error } = defineProps<{
 	error: NuxtError;
