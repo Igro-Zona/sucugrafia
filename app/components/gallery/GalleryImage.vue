2feda91d6175e28e35525aaf2b1676b52fba72ac
Igro-Zona <tim.bic22@gmail.com>
2025-12-27 05:43:30 +0300

    created GalleryImage component

---
+++ app/components/gallery/GalleryImage.vue
@@ -0,0 +1,27 @@
+<template>
+	<NuxtPicture
+		quality="60"
+		width="384"
+		height="216"
+		densities="1x 2x"
+		format="avif,webp"
+		legacy-format="jpg"
+		:src
+		provider="cloudinary"
+		class="block aspect-video w-96"
+		:img-attrs="{
+			alt: '',
+			class: 'rounded-xs object-cover w-full  h-full',
+			fetchpriority: 'low',
+			decoding: 'async',
+		}"
+		loading="lazy"
+		sizes="384px"
+	/>
+</template>
+
+<script setup lang="ts">
+const { src } = defineProps<{
+	src: string;
+}>();
+</script>
