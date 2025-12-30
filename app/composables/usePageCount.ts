c719e5538622c9360c2ed4ecf94502051f26808e
Igro-Zona <tim.bic22@gmail.com>
2025-12-30 21:09:01 +0300

    created usePageCount composable with data storing inside cookie

---
+++ app/composables/usePageCount.ts
@@ -0,0 +1,14 @@
+export default async function () {
+	const maxPagesCookie = useCookie<number | null>("pages_max", {
+		default: () => null,
+		maxAge: 86400,
+	});
+	const maxPagesCount = ref(maxPagesCookie.value);
+	if (!maxPagesCount.value) {
+		const { data } = await useFetch("/api/cloudinary-folders");
+		maxPagesCount.value = data.value ?? 1;
+		maxPagesCookie.value = maxPagesCount.value;
+	}
+
+	return maxPagesCount;
+}
