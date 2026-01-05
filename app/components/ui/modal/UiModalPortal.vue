ae7906d544c938f22504779f19f138065d7d94cb
Igro-Zona <tim.bic22@gmail.com>
2026-01-05 04:33:08 +0300

    created UiModal component for building flexible modals

---
+++ app/components/ui/modal/UiModalPortal.vue
@@ -0,0 +1,26 @@
+<template>
+	<DialogPortal
+		:to
+		:force-mount
+		:disabled
+		:defer
+	>
+		<slot />
+	</DialogPortal>
+</template>
+
+<script setup lang="ts">
+import { DialogPortal } from "reka-ui";
+
+const {
+	to = "body",
+	forceMount = false,
+	disabled = false,
+	defer = true,
+} = defineProps<{
+	to?: string | HTMLElement;
+	forceMount?: boolean;
+	disabled?: boolean;
+	defer?: boolean;
+}>();
+</script>
