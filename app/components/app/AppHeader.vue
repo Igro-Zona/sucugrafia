0b3997bc83810eebff067e8f7580187efcb62690
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-02-15 01:15:27 +0300

    created HeaderMenu component

--- app/components/app/AppHeader.vue
+++ app/components/app/AppHeader.vue
@@ -20,7 +20,7 @@
 			</div>
 
 			<div class="hidden lg:flex">
-				<UNavigationMenu :items />
+				<HeaderMenu :items />
 			</div>
 
 			<div class="flex items-center justify-end gap-1.5 lg:flex-1">
@@ -45,30 +45,21 @@
 		}"
 	>
 		<template #content>
-			<div class="flex h-(--ui-header-height) shrink-0 items-center justify-between gap-3 px-4 sm:px-6">
-				<div class="flex items-center gap-1.5 lg:flex-1">
-					<UiLink
-						to="/"
-						:events="true"
-					>
-						<UiLogo v-bind="logo" />
-					</UiLink>
-				</div>
+			<div class="flex h-(--ui-header-height) items-center justify-between gap-3 px-4 sm:px-6">
+				<UiLogo v-bind="logo" />
 
-				<div class="flex items-center justify-end gap-1.5 lg:flex-1">
-					<UButton
-						color="primary"
-						variant="link"
-						:aria-label="open ? 'Cerrar menú' : 'Abrir menú'"
-						:icon="open ? 'lucide:x' : 'lucide:menu'"
-						class="-me-1.5 cursor-pointer lg:hidden"
-						@click="toggleOpen"
-					/>
-				</div>
+				<UButton
+					color="primary"
+					variant="link"
+					:aria-label="open ? 'Cerrar menú' : 'Abrir menú'"
+					:icon="open ? 'lucide:x' : 'lucide:menu'"
+					class="-me-1.5 cursor-pointer lg:hidden"
+					@click="toggleOpen"
+				/>
 			</div>
 
 			<div class="overflow-y-auto p-4 sm:p-6">
-				<UNavigationMenu
+				<HeaderMenu
 					:items
 					orientation="vertical"
 					class="-mx-2.5"
@@ -80,7 +71,7 @@
 
 <script setup lang="ts">
 import type { UiLogoProps } from "../ui/UiLogo.vue";
-import type { NavigationMenuItem } from "@nuxt/ui";
+import type { HeaderMenuItem } from "../header/HeaderMenu.vue";
 import { twMerge, type ClassNameValue } from "tailwind-merge";
 
 export interface AppHeaderProps {
@@ -99,28 +90,36 @@ const logo: UiLogoProps = {
 	},
 };
 
-const items: NavigationMenuItem[] = [
+const items: HeaderMenuItem[] = [
 	{
 		label: "Inicio",
-		icon: "i-lucide-flag",
+		icon: {
+			name: "lucide:flag",
+		},
 		to: "/",
 		class: "text-md",
 	},
 	{
 		label: "Galeria",
-		icon: "i-lucide-images",
+		icon: {
+			name: "lucide:images",
+		},
 		to: "/galeria",
 		class: "text-md",
 	},
 	{
 		label: "Leer",
-		icon: "i-lucide-clipboard-list",
+		icon: {
+			name: "lucide:clipboard-list",
+		},
 		to: "/articulos",
 		class: "text-md",
 	},
 	{
 		label: "FAQ",
-		icon: "i-lucide-text-select",
+		icon: {
+			name: "lucide:text-select",
+		},
 		to: "/ayuda",
 		class: "text-md",
 	},
