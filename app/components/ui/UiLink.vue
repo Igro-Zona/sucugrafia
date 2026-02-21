c38049a03a98f318a2f72cd2de8abcf7fa8b17ca
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-02-21 18:53:18 +0300

    refactored UiLink component

--- app/components/ui/UiLink.vue
+++ app/components/ui/UiLink.vue
@@ -4,53 +4,74 @@
 		:data-active="isActive"
 		:external="external ?? isExternal"
 		:target="target ?? (external || isExternal ? '_blank' : undefined)"
-		:aria-current="ariaCurrent"
-		:class="[
-			twMerge('inline-flex gap-0.5 rounded-md', props.class),
-			ariaCurrent === 'page' && !(events ? events : false) && 'pointer-events-none cursor-default',
-		]"
+		:aria-current="resolveAriaCurrent"
+		:class="{
+			'group hover:text-highlighted focus-visible:text-default inline-flex cursor-pointer gap-1.5 rounded-md transition-colors':
+				!unstyled,
+			'pointer-events-none cursor-default': resolveAriaCurrent === 'page' && !events,
+		}"
 	>
-		<slot />
-		<Icon
-			v-if="(external || isExternal) && externalIcon"
-			v-bind="icon"
-		/>
+		<slot name="icon">
+			<Icon
+				v-if="icon"
+				v-bind="normalizeIconProps(icon)"
+			/>
+		</slot>
+
+		<span
+			v-if="label || $slots.default || externalIcon || $slots.externalIcon"
+			class="inline-flex gap-0.5 truncate transition-colors"
+		>
+			<slot>
+				{{ label }}
+			</slot>
+
+			<slot name="externalIcon">
+				<Icon
+					v-if="(external || isExternal) && externalIcon"
+					v-bind="
+						normalizeIconProps(typeof externalIcon === 'boolean' ? { name: 'lucide:arrow-up-right', size: 16 } : externalIcon)
+					"
+				/>
+			</slot>
+		</span>
+
+		<slot name="trailingIcon">
+			<Icon
+				v-if="trailingIcon"
+				v-bind="normalizeIconProps(trailingIcon)"
+			/>
+		</slot>
 	</NuxtLink>
 </template>
 
 <script setup lang="ts">
 import type { NuxtLinkProps } from "#app";
-import { twMerge, type ClassNameValue } from "tailwind-merge";
+import type { AriaAttributes } from "vue";
+import type { StyledComponentProps } from "~/types/Components";
+import { reactiveOmit } from "@vueuse/core";
 
-export interface UiLinkProps extends Omit<NuxtLinkProps, "noPrefetch"> {
+export interface UiLinkProps extends Omit<NuxtLinkProps, "noPrefetch">, StyledComponentProps {
+	label?: string;
 	events?: boolean;
-	externalIcon?: boolean;
-	icon?: IconProps;
-	class?: ClassNameValue;
+	icon?: string | IconProps;
+	externalIcon?: boolean | string | IconProps;
+	trailingIcon?: string | IconProps;
+	ariaCurrent?: AriaAttributes["aria-current"];
 }
 
-const props = withDefaults(defineProps<UiLinkProps>(), {
-	events: false,
-	externalIcon: true,
-	icon: () => ({ name: "lucide:arrow-up-right", size: 16 }),
-	class: undefined,
-});
-const { events, externalIcon, icon, class: _class, ...linkProps } = props;
-
-const isExternal = computed(() => isExternalUrl(props.to?.toString()));
+const { normalizeIconProps } = useIcon();
+const props = defineProps<UiLinkProps>();
+const linkProps = reactiveOmit(props, ["label", "events", "icon", "externalIcon", "trailingIcon", "unstyled", "ariaCurrent"]);
 
 const route = useRoute();
-const ariaCurrent = computed(() => {
-	const targetPath = props.to?.toString();
-	if (!targetPath) return undefined;
-	if (route.path === targetPath) return "page";
-	return route.path.startsWith(targetPath) ? "step" : undefined;
+const targetPath = computed(() => props.to?.toString());
+const resolveAriaCurrent = computed(() => {
+	if (props.ariaCurrent) return props.ariaCurrent;
+	if (!targetPath.value) return undefined;
+	if (route.path === targetPath.value) return "page";
+	return route.path.startsWith(targetPath.value) ? "step" : undefined;
 });
-const isActive = computed(() => {
-	const targetPath = props.to?.toString();
-	if (!targetPath) return false;
-	const exact = route.path === targetPath;
-	const active = route.path.startsWith(targetPath);
-	return exact && active;
-});
+const isActive = computed(() => route.path === targetPath.value);
+const isExternal = computed(() => isExternalUrl(targetPath.value));
 </script>
