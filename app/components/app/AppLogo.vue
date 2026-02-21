f3f3c19170bb1db16b5ae03bf12393613003425a
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-02-21 18:54:01 +0300

    updated css styles, created AppLogo component

---
+++ app/components/app/AppLogo.vue
@@ -0,0 +1,24 @@
+<template>
+	<UiLink
+		to="/"
+		:events="true"
+	>
+		<Primitive
+			:as="as"
+			class="text-highlighted flex items-center gap-1 text-2xl font-semibold"
+		>
+			<Icon
+				name="sucua:book"
+				:size="34"
+				class="text-primary"
+			/>
+			<span>Sucu<span class="text-primary">grafia</span></span>
+		</Primitive>
+	</UiLink>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	as?: AsPropDefault;
+}>();
+</script>
