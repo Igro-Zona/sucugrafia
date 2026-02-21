91c0052a946f916ab4cb3bae76e57b76107c7159
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-02-21 19:53:59 +0300

    refactored AppRoot and AppMain components

--- app/components/app/AppRoot.vue
+++ app/components/app/AppRoot.vue
@@ -1,10 +1,5 @@
 <template>
-	<ConfigProvider
-		:use-id="() => useId()"
-		:dir="locale?.dir"
-		:locale="locale?.code"
-		:scroll-body="scrollBody"
-	>
+	<ConfigProvider :use-id="() => useId()">
 		<NuxtLoadingIndicator
 			color="#00dc82"
 			:height="4"
@@ -14,27 +9,3 @@
 		<slot />
 	</ConfigProvider>
 </template>
-
-<script setup lang="ts">
-import { ConfigProvider } from "reka-ui";
-
-export type Locale<T> = {
-	name: string;
-	code: string;
-	dir: "rtl" | "ltr";
-	messages: T;
-};
-export interface AppRootProps {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	locale?: Locale<any>;
-	scrollBody?:
-		| boolean
-		| {
-				padding?: string | number | boolean;
-
-				margin?: string | number | boolean;
-		  };
-}
-
-defineProps<AppRootProps>();
-</script>
