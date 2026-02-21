2794a987376271d5aea0d948019c6ac7e0959f58
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-02-22 02:21:08 +0300

    refactored index page

--- app/components/hero/HeroContent.vue
+++ app/components/hero/HeroContent.vue
@@ -1,41 +1,41 @@
 <template>
-	<section
-		:aria-label="title"
-		class="relative z-9 space-y-8 px-4 py-24 backdrop-blur-xs transition-all duration-1000 hover:backdrop-blur-none sm:px-6 sm:py-32 lg:px-8 lg:py-40"
+	<UiSection
+		:title="title"
+		class="relative z-1 space-y-8! px-4 py-24 backdrop-blur-xs transition-all duration-1000 hover:backdrop-blur-none sm:px-6 sm:py-32 lg:px-8 lg:py-40"
 	>
-		<hgroup class="text-center">
-			<h2 class="text-primary text-4xl font-semibold tracking-tighter sm:text-6xl md:text-7xl">
-				{{ title }}
-			</h2>
-			<p class="text-highlighted mt-6 tracking-tight text-balance sm:text-xl sm:font-semibold">
-				{{ description }}
-			</p>
-		</hgroup>
+		<template #title>
+			<hgroup class="text-center">
+				<h2 class="text-primary text-4xl font-semibold tracking-tighter sm:text-6xl md:text-7xl">
+					{{ title }}
+				</h2>
+				<p class="text-highlighted mt-6 tracking-tight text-balance sm:text-xl sm:font-semibold">
+					{{ description }}
+				</p>
+			</hgroup>
+		</template>
 
 		<div class="flex flex-wrap items-center justify-center gap-6">
 			<UiLink
 				to="/galeria"
-				class="text-inverted! bg-primary hover:bg-primary/75 active:bg-primary/75 cursor-pointer items-center gap-2 px-3 py-2"
-			>
-				<Icon name="i-lucide-image" />
-				<span>Ver galeria</span>
-			</UiLink>
+				class="text-inverted! bg-primary hover:bg-primary/75 active:bg-primary/75 px-3 py-2"
+				icon="lucide:image"
+				label="Ver galeria"
+			/>
 
 			<UiLink
 				to="/articulos"
-				class="text-default! bg-muted border-accented hover:bg-muted/75 active:bg-muted/75 cursor-pointer items-center gap-2 border px-3 py-2"
-			>
-				<span>Leer articulos</span>
-				<Icon name="i-lucide-arrow-right" />
-			</UiLink>
+				class="text-default! bg-muted border-accented hover:bg-muted/75 active:bg-muted/75 border px-3 py-2"
+				label="Leer articulos"
+				trailing-icon="lucide:arrow-right"
+			/>
 		</div>
-	</section>
+	</UiSection>
 </template>
 
 <script setup lang="ts">
 export interface HeroContentProps {
-	title?: string;
-	description?: string;
+	title: string;
+	description: string;
 }
 
 defineProps<HeroContentProps>();
