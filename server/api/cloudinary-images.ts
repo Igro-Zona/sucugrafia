9a07f08bd077458054c82c18c1806333b6214254
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-04-26 18:04:02 +0300

    removed image caching

--- server/api/cloudinary-images.ts
+++ server/api/cloudinary-images.ts
@@ -1,28 +1,25 @@
-export default defineCachedEventHandler(
-	async (event) => {
-		setHeader(event, "Cache-Control", "public, max-age=86400, s-maxage=86400");
+export default defineEventHandler(async (event) => {
+	setHeader(event, "Cache-Control", "public, max-age=86400, s-maxage=86400");
 
-		const config = useRuntimeConfig(event);
-		const query = getQuery(event);
-		const page = query.page ? Number(query.page) : 1;
-		const res = await $fetch<{ resources: { public_id: string; context?: { custom: { alt?: string; caption?: string } } }[] }>(
-			`https://api.cloudinary.com/v1_1/${config.cloudinaryCloudName}/resources/by_asset_folder`,
-			{
-				method: "GET",
-				headers: {
-					Authorization: "Basic " + Buffer.from(`${config.cloudinaryApiKey}:${config.cloudinaryApiSecret}`).toString("base64"),
-				},
-				query: {
-					asset_folder: `gallery/${page}`,
-					max_results: 48,
-				},
+	const config = useRuntimeConfig(event);
+	const query = getQuery(event);
+	const page = query.page ? Number(query.page) : 1;
+	const res = await $fetch<{ resources: { public_id: string; context?: { custom: { alt?: string; caption?: string } } }[] }>(
+		`https://api.cloudinary.com/v1_1/${config.cloudinaryCloudName}/resources/by_asset_folder`,
+		{
+			method: "GET",
+			headers: {
+				Authorization: "Basic " + Buffer.from(`${config.cloudinaryApiKey}:${config.cloudinaryApiSecret}`).toString("base64"),
 			},
-		);
-		return res.resources.map((image) => ({
-			src: image.public_id,
-			title: image.context?.custom.caption,
-			description: image.context?.custom.alt,
-		}));
-	},
-	{ maxAge: 86400, staleMaxAge: 86400 },
-);
+			query: {
+				asset_folder: `gallery/${page}`,
+				max_results: 48,
+			},
+		},
+	);
+	return res.resources.map((image) => ({
+		src: image.public_id,
+		title: image.context?.custom.caption,
+		description: image.context?.custom.alt,
+	}));
+});
